53e4f21a715a4a743e93ed6252239e24d955ad10
RTX <2393482408@qq.com>
2017-09-04 22:56:13 +0800

    en

--- TornadoStudy/methods/db.py
+++ TornadoStudy/methods/db.py
@@ -12,6 +12,7 @@
 # 数据库链接
 import pymysql
 
-db = pymysql.connect("192.168.56.101", "root", "shopncpw", "easycms")
+# db = pymysql.connect("192.168.56.101", "root", "shopncpw", "easycms")
+db = pymysql.connect("192.168.56.102", "root", "shopncpw", "cms")
 # 使用 cursor() 方法创建一个游标对象 cursor
 cursor = db.cursor()
